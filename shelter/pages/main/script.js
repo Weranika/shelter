ce2209fd628c2498acf960d07e09c887c7010f2d
veranikal <hermony.grenger@gmail.com>
2022-04-24 14:18:34 +0200

    feat: burger-menu in main

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,12 +1,64 @@
-function burgerNavigation(x) {
-    document.getElementById("navigation-bar").classList.toggle("change");  
-    //document.getElementById("navigation").classList.toggle("nonshow");
-    document.getElementById("logo").classList.toggle("nonshow");
-    x.classList.toggle("rotate");       
-      
-    if (document.getElementById('navigation').style.display == 'block') {
-        document.getElementById('navigation').style.display = 'none';
+// import { json } from "../../js/json.js";
+// json = require('../../js/json.js');
+console.log(1);
+console.log(json);
+function burgerNavigation(elem) {
+    let navigationBar = document.getElementById("navigation-bar");
+    let popup = document.getElementById("popup-background");
+    let navigation = document.getElementById('navigation');
+
+    //navigationBar.classList.toggle("change"); 
+    if (navigationBar.classList.contains("change")) {
+        navigationBar.classList.remove("change");
+        navigationBar.classList.add("slideout");
     } else {
-        document.getElementById('navigation').style.display = 'block';
+        navigationBar.classList.remove("slideout");
+        navigationBar.classList.add("change");
+    }
+    popup.classList.toggle("nonshow");   
+    document.getElementById("logo").classList.toggle("nonshow");
+    elem.classList.toggle("rotate");       
+      
+    if (navigation.style.display == 'block') {
+        navigation.style.display = 'none';
+    } else {
+        navigation.style.display = 'block';
     }
 }
+document.getElementById("popup-background").addEventListener("click", handler1);
+function handler1() {       
+    let navigation = document.getElementById('navigation');
+
+    document.getElementById("navigation-bar").classList.remove("change");
+    document.getElementById("navigation-bar").classList.add("slideout");
+    document.getElementById("popup-background").classList.toggle("nonshow");
+    document.getElementById("burger").classList.toggle("rotate");    
+    document.getElementById("logo").classList.toggle("nonshow");
+    if (navigation.style.display == 'block') {
+        navigation.style.display = 'none';
+    } else {
+        navigation.style.display = 'block';
+    }
+}
+document.getElementById("aboutTheScelter").addEventListener("click", handler2);
+function handler2() {
+    let navigationBar = document.getElementById("navigation-bar");    
+    let navigation = document.getElementById('navigation');
+    
+    if (navigationBar.classList.contains("slideout")) {
+        navigationBar.classList.remove("slideout");
+        navigationBar.classList.add("change");
+    } else {
+        navigationBar.classList.remove("change");
+        navigationBar.classList.add("slideout");
+    }
+    document.getElementById("popup-background").classList.toggle("nonshow");
+    document.getElementById("burger").classList.toggle("rotate");    
+    document.getElementById("logo").classList.toggle("nonshow");
+    if (navigation.style.display == 'block') {
+        navigation.style.display = 'none';
+    } else {
+        navigation.style.display = 'block';
+    }
+}
+
